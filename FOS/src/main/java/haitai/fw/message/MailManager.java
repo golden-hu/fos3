c53b4eca0cf00a050693c58c3e5a5730f9368c91
Yonggang Guo <yonggang.1.guo@nokia.com>
2011-10-19 22:33:40 +0800

    clean code

--- FOS/src/main/java/haitai/fw/message/MailManager.java
+++ FOS/src/main/java/haitai/fw/message/MailManager.java
@@ -31,26 +31,20 @@ public class MailManager {
 	private IPMessageDAO dao;
 	private static FosLogger logger = new FosLogger(MailManager.class);
 
-	public void setSender(JavaMailSender sender) {
-		this.sender = sender;
-	}
-
 	@Transactional
 	public void sendEmail(PMessage message) {
 		SessionManager.setAttr(SessionKeyType.COMPCODE, message.getCompCode());
 		send(message);
 		message.setMessSendFlag(ConstUtil.TrueShort);
 		dao.updateSendFlag(message);
-		logger.info("message id = " + message.getMessId()
-				+ ", send flag update success!");
+		logger.info("message id = " + message.getMessId() + ", send flag update success!");
 	}
 
 	public void send(PMessage pmsg) {
 		MimeMessage message = sender.createMimeMessage();
 		MimeMessageHelper helper;
 		try {
-			helper = new MimeMessageHelper(message, true,
-					ConstUtil.XML_ENCODING_UTF8);
+			helper = new MimeMessageHelper(message, true, ConstUtil.XML_ENCODING_UTF8);
 			String[] to = splitEmailAddress(pmsg.getMessTo());
 			String[] cc = splitEmailAddress(pmsg.getMessCc());
 			String[] bcc = splitEmailAddress(pmsg.getMessBcc());
@@ -62,24 +56,19 @@ public class MailManager {
 				helper.setBcc(bcc);
 			}
 			String subject = pmsg.getMessSubject();
-			if(StringUtil.isBlank(subject)) {
+			if (StringUtil.isBlank(subject)) {
 				subject = "no subject";
 			}
-			helper.setFrom("fos");			
+			helper.setFrom("fos");
 			helper.setReplyTo(pmsg.getMessFrom());
 			helper.setSubject(subject);
-			helper.setText(pmsg.getMessBody(),
-					pmsg.getMessType() == ConstUtil.MESS_TYPE_FAX ? false
-							: true);
+			helper.setText(pmsg.getMessBody(), pmsg.getMessType() != ConstUtil.MESS_TYPE_FAX);
 			if (StringUtil.isNotBlank(pmsg.getMessAttachment())) {
 				String fileName = pmsg.getMessAttachment();
 				FileUtil.createDirs(ConfigUtil.getRealTempDir());
-				fileName = ConfigUtil.getRealTempDir()
-						+ ConstUtil.DIR_SEP + fileName;
-				FileSystemResource file = new FileSystemResource(new File(
-						fileName));
-				helper.addAttachment(
-						MimeUtility.encodeWord(file.getFilename()), file);
+				fileName = ConfigUtil.getRealTempDir() + ConstUtil.DIR_SEP + fileName;
+				FileSystemResource file = new FileSystemResource(new File(fileName));
+				helper.addAttachment(MimeUtility.encodeWord(file.getFilename()), file);
 			}
 		} catch (MessagingException e) {
 			logger.error("mail id = " + pmsg.getMessId() + ", send failed", e);
@@ -90,9 +79,9 @@ public class MailManager {
 		}
 		sender.send(message);
 	}
-	
-	private String[] splitEmailAddress(String s){
-		if(StringUtil.isBlank(s)){
+
+	private String[] splitEmailAddress(String s) {
+		if (StringUtil.isBlank(s)) {
 			return null;
 		}
 		s = s.replaceAll(",", ";");
