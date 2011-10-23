dd06fe26cecacba24f1254ff66ee289b22401cfe
Yonggang Guo <yonggang.1.guo@nokia.com>
2011-10-23 09:55:42 +0800

    clear code

--- FOS/src/main/java/haitai/fos/exp/service/TInvoiceItemService.java
+++ FOS/src/main/java/haitai/fos/exp/service/TInvoiceItemService.java
@@ -5,39 +5,35 @@ import haitai.fos.exp.entity.table.TInvoiceItem;
 import haitai.fw.exception.BusinessException;
 import haitai.fw.util.ConstUtil;
 import haitai.fw.util.MessageUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 @Service
 public class TInvoiceItemService {
 	
-	private ITInvoiceItemDAO dao = null;
+	@Autowired
+	private ITInvoiceItemDAO dao;
 	
 	@Transactional
 	public List<TInvoiceItem> save(List<TInvoiceItem> entityList) {
 		List<TInvoiceItem> retList = new ArrayList<TInvoiceItem>();
 		for (TInvoiceItem entity : entityList) {
-			if (ConstUtil.ROW_N
-					.equalsIgnoreCase(entity.getRowAction())) {
+			if (ConstUtil.ROW_N.equalsIgnoreCase(entity.getRowAction())) {
 				dao.save(entity);
 				retList.add(entity);
-			} else if (ConstUtil.ROW_M.equalsIgnoreCase(entity
-					.getRowAction())) {
+			} else if (ConstUtil.ROW_M.equalsIgnoreCase(entity.getRowAction())) {
 				retList.add(dao.update(entity));
-			} else if (ConstUtil.ROW_R.equalsIgnoreCase(entity
-					.getRowAction())) {
+			} else if (ConstUtil.ROW_R.equalsIgnoreCase(entity.getRowAction())) {
 				TInvoiceItem delEntity = dao.findById(entity.getInitId());
 				delEntity.setRowAction(ConstUtil.ROW_R);
 				dao.update(delEntity);
 			} else {
-				throw new BusinessException(
-						MessageUtil.FW_ERROR_ROW_ACTION_NULL);
+				throw new BusinessException(MessageUtil.FW_ERROR_ROW_ACTION_NULL);
 			}
 		}
 		return retList;
@@ -49,12 +45,4 @@ public class TInvoiceItemService {
 		return dao.findByProperties(queryMap);
 	}	
 	
-	public ITInvoiceItemDAO getDao() {
-		return dao;
-	}
-
-	@Autowired
-	public void setDao(ITInvoiceItemDAO dao) {
-		this.dao = dao;
-	}
 }
