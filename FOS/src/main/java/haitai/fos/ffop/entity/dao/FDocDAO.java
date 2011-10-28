03b2f69ad06e0d51c94923097a786ea88e066836
Yonggang Guo <yonggang.1.guo@nokia.com>
2011-10-28 23:47:44 +0800

    clear code

--- FOS/src/main/java/haitai/fos/ffop/entity/dao/FDocDAO.java
+++ FOS/src/main/java/haitai/fos/ffop/entity/dao/FDocDAO.java
@@ -8,16 +8,14 @@ import haitai.fw.entity.GenericDAO;
 import haitai.fw.entity.HttpHeader;
 import haitai.fw.util.CompanyConfigUtil;
 import haitai.fw.util.ConstUtil;
-
-import java.util.List;
-import java.util.Map;
+import org.springframework.orm.jpa.JpaCallback;
+import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceException;
 import javax.persistence.Query;
-
-import org.springframework.orm.jpa.JpaCallback;
-import org.springframework.stereotype.Component;
+import java.util.List;
+import java.util.Map;
 
 @Component
 public class FDocDAO extends GenericDAO<FDoc, Integer> implements IFDocDAO {
@@ -27,51 +25,40 @@ public class FDocDAO extends GenericDAO<FDoc, Integer> implements IFDocDAO {
 	}
 
 	@SuppressWarnings("unchecked")
-	public List complexQuery(final List<FosQuery> conditions,
-			final Map<String, Object> propertyMap) {
+	public List complexQuery(final List<FosQuery> conditions, final Map<String, Object> propertyMap) {
 		final Class t1 = FDoc.class;
 		final Class t2 = FConsign.class;
 		String joinSql = "t1.consId = t2.consId";
 		List retList = complexQuery(conditions, propertyMap, "t1,t2", joinSql, t1, t2);
 
-		String rowCount = String.valueOf(complexQuerySize(conditions,
-				propertyMap, "distinct t1", joinSql, t1, t2));
+		String rowCount = String.valueOf(complexQuerySize(conditions, propertyMap, "distinct t1", joinSql, t1, t2));
 		propertyMap.put(HttpHeader.ROWCOUNT, rowCount);
 		return retList;
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	public List<FDoc> complexQueryNeedRelease(final List<FosQuery> conditions,
-			final Map<String, Object> propertyMap) {
+	public List<FDoc> complexQueryNeedRelease(final List<FosQuery> conditions, final Map<String,
+			Object> propertyMap) {
 		final Class t1 = FDoc.class;
 		final Class t2 = FConsign.class;
-		String blId = CompanyConfigUtil
-				.getCompanyConfig(ConstUtil.COMCF_FDOC_BL);
-		String ccId = CompanyConfigUtil
-				.getCompanyConfig(ConstUtil.COMCF_FDOC_CC);
-		String joinSql = "t1.consId = t2.consId and (t1.dotyId = " + blId
-				+ " or t1.dotyId = " + ccId + ")";
-		List retList = complexQuery(conditions, propertyMap, "t1", joinSql, t1,
-				t2);
-		return retList;
+		String blId = CompanyConfigUtil.getCompanyConfig(ConstUtil.COMCF_FDOC_BL);
+		String ccId = CompanyConfigUtil.getCompanyConfig(ConstUtil.COMCF_FDOC_CC);
+		String joinSql = "t1.consId = t2.consId and (t1.dotyId = " + blId + " or t1.dotyId = " + ccId + ")";
+		return complexQuery(conditions, propertyMap, "t1", joinSql, t1, t2);
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	public List<FDoc> complexQueryNeedAlert(final List<FosQuery> conditions,
-			final Map<String, Object> propertyMap){
+	public List<FDoc> complexQueryNeedAlert(final List<FosQuery> conditions, final Map<String, Object> propertyMap) {
 		final Class<FDoc> t1 = FDoc.class;
 		final Class<FConsign> t2 = FConsign.class;
 		String joinSql = "t1.consId = t2.consId and (t1.alertFlag is null or t1.alertFlag = '0')";
-		List retList = complexQuery(conditions, propertyMap, "t1", joinSql, t1,
-				t2);
-		return retList;
-
+		return complexQuery(conditions, propertyMap, "t1", joinSql, t1, t2);
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	@Override
 	public void updateConsNoByConsId(final Integer consId, final String consNo) {
-		StringBuffer sb = new StringBuffer();
+		StringBuilder sb = new StringBuilder();
 		sb.append("update FDoc t1 set ");
 		sb.append("t1.consNo = ?, t1.version = t1.version + 1 ");
 		sb.append("where t1.consId = ? and t1.removed = 0");
