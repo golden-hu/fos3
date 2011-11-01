59ddd5aaf217b50d98d77105c3a4ec5671bf4bee
Yonggang Guo <yonggang.1.guo@nokia.com>
2011-11-01 22:54:46 +0800

    remove controller

--- FOS/src/main/java/haitai/fos/general/service/GChargeService.java
+++ FOS/src/main/java/haitai/fos/general/service/GChargeService.java
@@ -1,12 +1,20 @@
 package haitai.fos.general.service;
 
+import haitai.fos.ffse.service.SExpenseService;
 import haitai.fos.general.entity.idao.IGChargeDAO;
 import haitai.fos.general.entity.table.GCharge;
+import haitai.fos.sys.entity.table.PUserExpePermission;
 import haitai.fw.entity.FosQuery;
+import haitai.fw.util.CompanyConfigUtil;
+import haitai.fw.util.ConstUtil;
+import haitai.fw.util.SpringContextHolder;
+import haitai.fw.util.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -14,6 +22,8 @@ import java.util.Map;
 public class GChargeService implements IGeneralService<GCharge> {
 	@Autowired
 	private IGChargeDAO dao;
+	@Autowired
+	private SExpenseService expenseService;
 
 	@Transactional
 	public List<GCharge> save(List<GCharge> itemList) {
@@ -28,5 +38,53 @@ public class GChargeService implements IGeneralService<GCharge> {
 
 	public List<GCharge> complexQuery(List<FosQuery> conditions, Map<String, Object> queryMap) {
 		return dao.complexQuery(conditions, queryMap);
+	}
+
+	@Transactional(readOnly = true)
+	public List<GCharge> queryChargeCommission(Map<String, Object> queryMap) {
+		List<GCharge> retList = new ArrayList<GCharge>();
+		String commissionCNY = CompanyConfigUtil.getCompanyConfig("COMMISSION_CHAR_CNY");
+		String commissionUSD = CompanyConfigUtil.getCompanyConfig("COMMISSION_CHAR_USD");
+		ApplicationContext ctx = SpringContextHolder.getContext();
+		if (StringUtil.isNotBlank(commissionUSD)) {
+			GCharge usd = dao.findById(Integer.parseInt(commissionUSD));
+			if (usd != null) {
+				retList.add(usd);
+			}
+		}
+		if (StringUtil.isNotBlank(commissionCNY)) {
+			GCharge cny = dao.findById(Integer.parseInt(commissionCNY));
+			if (cny != null) {
+				retList.add(cny);
+			}
+		}
+		return retList;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Transactional(readOnly = true)
+	public List<GCharge> queryChargeFilted(Map<String, Object> queryMap) {
+		String expeType = (String) queryMap.get("expeType");
+		List<GCharge> retList = new ArrayList<GCharge>();
+		List<GCharge> oriList = query(queryMap);
+		Map<String, PUserExpePermission> permMap = expenseService.getExpePermissionMap();
+		String key = expeType + ConstUtil.USEP_CHCL_ALL;
+		if (permMap.containsKey(key) && isEdit(permMap.get(key))) {
+			retList.addAll(oriList);
+		} else {
+			for (GCharge charge : oriList) {
+				key = expeType + charge.getChclId();
+				if (permMap.containsKey(key) && isEdit(permMap.get(key))) {
+					retList.add(charge);
+				}
+			}
+		}
+
+		return retList;
+	}
+
+	private boolean isEdit(PUserExpePermission perm) {
+		return ConstUtil.TrueShort.equals(perm.getUsepEditable())
+				|| ConstUtil.TrueShort.equals(perm.getUsepEditAll());
 	}
 }
--- FOS/src/main/java/haitai/fos/general/service/GUnitService.java
+++ FOS/src/main/java/haitai/fos/general/service/GUnitService.java
@@ -1,12 +1,15 @@
 package haitai.fos.general.service;
 
+import haitai.fos.general.entity.idao.IGContainerTypeDAO;
 import haitai.fos.general.entity.idao.IGUnitDAO;
+import haitai.fos.general.entity.table.GContainerType;
 import haitai.fos.general.entity.table.GUnit;
 import haitai.fw.entity.FosQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -14,6 +17,8 @@ import java.util.Map;
 public class GUnitService implements IGeneralService<GUnit> {
 	@Autowired
 	private IGUnitDAO dao;
+	@Autowired
+	private IGContainerTypeDAO containerTypeDao;
 
 	@Transactional
 	public List<GUnit> save(List<GUnit> itemList) {
@@ -28,5 +33,28 @@ public class GUnitService implements IGeneralService<GUnit> {
 
 	public List<GUnit> complexQuery(List<FosQuery> conditions, Map<String, Object> queryMap) {
 		return null;
+	}
+
+	/**
+	 * 把ContainerType转换成Unit, 跟Unit拼起来返回
+	 *
+	 * @param queryMap the query map
+	 * @return the unit list
+	 */
+	@Transactional(readOnly = true)
+	public List<GUnit> queryUnitPlusContType(Map<String, Object> queryMap) {
+		List<GUnit> retList = new ArrayList<GUnit>();
+		retList.addAll(dao.findByProperties(queryMap));
+		List<GContainerType> contList = containerTypeDao.findByProperties(queryMap);
+		Integer id = -1;
+		for (GContainerType item : contList) {
+			GUnit unit = new GUnit();
+			unit.setUnitId(id);
+			unit.setUnitCode(item.getCotyCode());
+			unit.setUnitName(item.getCotyCode());
+			id--;
+			retList.add(unit);
+		}
+		return retList;
 	}
 }
