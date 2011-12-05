df30da92acec0a1336f5512b9c53cc2446d6db16
Yonggang Guo <yonggang.1.guo@nokia.com>
2011-12-05 13:30:50 +0800

    fix can't delete role function

--- FOS/src/main/java/haitai/fos/sys/service/PRoleFunctionService.java
+++ FOS/src/main/java/haitai/fos/sys/service/PRoleFunctionService.java
@@ -2,10 +2,13 @@ package haitai.fos.sys.service;
 
 import haitai.fos.sys.entity.idao.IPRoleFunctionDAO;
 import haitai.fos.sys.entity.table.PRoleFunction;
+import haitai.fw.exception.BusinessException;
+import haitai.fw.util.RowAction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -16,7 +19,21 @@ public class PRoleFunctionService {
 
 	@Transactional
 	public List<PRoleFunction> save(List<PRoleFunction> entityList) {
-		return dao.saveByRowAction(entityList);
+		List<PRoleFunction> retList = new ArrayList<PRoleFunction>();
+		for (PRoleFunction entity : entityList) {
+			if (entity.getRowAction() == RowAction.N) {
+				entity.setRofuId(null);
+				dao.save(entity);
+				retList.add(entity);
+			} else if (entity.getRowAction() == RowAction.M) {
+				retList.add(dao.update(entity));
+			} else if (entity.getRowAction() == RowAction.R) {
+				dao.delete(entity.getRofuId());
+			} else {
+				throw new BusinessException("fw.row_action_null");
+			}
+		}
+		return retList;
 	}
 
 	@SuppressWarnings("unchecked")
