662d42313070a12080ee1ebac35b8eccd07d3c29
Gordon <906046472@qq.com>
2012-04-13 19:50:40 +0800

    创建表F_CUSTOMS_TYPE报关类型

--- FOS/sql/fos.sql
+++ FOS/sql/fos.sql
@@ -1421,6 +1421,22 @@ CREATE TABLE IF NOT EXISTS `F_CUSTOMS_ENTRY` (
   PRIMARY KEY  (`CUEN_ID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+
+CREATE TABLE F_CUSTOMS_TYPE(
+     ID              INT             AUTO_INCREMENT,
+     CUTY_NAME       VARCHAR(32),
+     CUTY_CODE       INT,
+     ACTIVE          TINYINT,
+     CREATE_BY       INT,
+     CREATE_TIME     DATETIME,
+     MODIFY_BY       INT,
+     MODIFY_TIME     DATETIME,
+     COMP_CODE       CHAR(4)    NOT NULL,
+     VERSION         INT  NOT NULL default '0',
+     REMOVED         TINYINT  NOT NULL default '0',
+     PRIMARY KEY (ID)
+)ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 -- --------------------------------------------------------
 
 --
