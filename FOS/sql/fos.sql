9b6d5a3cb81858b7817dba2fe78c4630993412e5
simon <zhouxu.huang@gmail.com>
2012-05-09 18:09:38 +0800

    sql update

--- FOS/sql/fos.sql
+++ FOS/sql/fos.sql
@@ -250,6 +250,23 @@ CREATE TABLE IF NOT EXISTS `C_CUSTOMER_CONTACT` (
   PRIMARY KEY  (`CUCO_ID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+CREATE TABLE C_CUSTOMER_SITE(
+    CUSI_ID        INT             AUTO_INCREMENT,
+    CUST_ID        INT             NOT NULL,
+    CUSI_NAME      VARCHAR(200),
+    CUSI_TEL varchar(32) collate utf8_unicode_ci default NULL,
+    CUSI_CONTACT varchar(32) collate utf8_unicode_ci default NULL,
+    CUSI_ADDRESS varchar(200) collate utf8_unicode_ci default NULL,
+    CUSI_TYPE      INT,    
+    CREATE_BY      INT,
+    CREATE_TIME    DATETIME,
+    MODIFY_BY      INT,
+    MODIFY_TIME    DATETIME,
+    COMP_CODE      CHAR(4)         NOT NULL,
+    VERSION        INT             DEFAULT 0 NOT NULL,
+    REMOVED        TINYINT         DEFAULT 0 NOT NULL,
+    PRIMARY KEY (CUSI_ID)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 -- --------------------------------------------------------
 
 --
