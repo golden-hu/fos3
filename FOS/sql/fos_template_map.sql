9cecf70082db5656cc80a1a2434d129a3c8398e3
simon <zhouxu.huang@gmail.com>
2013-09-01 23:24:05 +0800

    同步SQL

--- FOS/sql/fos_template_map.sql
+++ FOS/sql/fos_template_map.sql
@@ -539,7 +539,8 @@ INSERT INTO `P_TEMPLATE_MAP` (`TETY_ID`, `TEMA_NAME`, `TEMA_TABLE`, `TEMA_FIELD`
 (18, '报关行地址', 'FConsign', 'consCustomsAddress', NULL),
 (18, '报关公司联系人', 'FConsign', 'consCustomsContact', NULL),
 (18, '报关公司电话', 'FConsign', 'consCustomsTel', NULL),
-(18, '报关行', 'FConsign', 'consCustomsVendorName', NULL);
+(18, '报关行', 'FConsign', 'consCustomsVendorName', NULL),
+(18, '运输方式', 'FConsign', 'tranCode', NULL);
 
 
 
