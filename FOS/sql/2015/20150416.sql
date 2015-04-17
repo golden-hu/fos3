72186dd9a8a86cb092620887debdd5c1da7cc49c
simon <simon@simon-huang.lan>
2015-04-17 15:09:52 +0800

    fix bugs

--- FOS/sql/2015/20150416.sql
+++ FOS/sql/2015/20150416.sql
@@ -1,6 +1,6 @@
 INSERT INTO `P_TEMPLATE_MAP` (`TETY_ID`, `TEMA_NAME`, `TEMA_TABLE`, `TEMA_FIELD`, `TEMA_CONVERTER`) VALUES
 (26, '到港日期', 'FConsign', 'consEta', NULL),
-(26, '驳船船名', 'FConsign', 'consBVessl', NULL),
+(26, '驳船船名', 'FConsign', 'consBVessel', NULL),
 (26, '驳船航次', 'FConsign', 'consBVoyage', NULL),
 (26, '驳船ETD', 'FConsign', 'consBEtd', NULL),
 (26, '驳船ETA', 'FConsign', 'consBEta', NULL)
--- FOS/sql/fos_template_map.sql
+++ FOS/sql/fos_template_map.sql
@@ -783,7 +783,8 @@ INSERT INTO `P_TEMPLATE_MAP` (`TETY_ID`, `TEMA_NAME`, `TEMA_TABLE`, `TEMA_FIELD`
 (26, '委托日期', 'FConsign', 'consDate', NULL),
 (26, '箱号', 'FConsign', 'consContainerNo', NULL),
 (26, '箱封号', 'FConsign', 'consSealNo', NULL),
-(26, '驳船船名', 'FConsign', 'consBVessl', NULL),
+(26, '到港日期', 'FConsign', 'consEta', NULL),
+(26, '驳船船名', 'FConsign', 'consBVessel', NULL),
 (26, '驳船航次', 'FConsign', 'consBVoyage', NULL),
 (26, '驳船ETD', 'FConsign', 'consBEtd', NULL),
 (26, '驳船ETA', 'FConsign', 'consBEta', NULL)
