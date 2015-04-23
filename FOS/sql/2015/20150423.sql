ec3042641f5bd9c341a2ee97d83f5e94e4bbe5dc
yinyulong <git@git.hitisoft.com:other/zhiyouoa.git>
2015-04-23 09:49:41 +0800

    模板bug修正，添加英文日期转换方法。

---
+++ FOS/sql/2015/20150423.sql
@@ -0,0 +1,2 @@
+-- add by yinyulong 付费方式字段导出错误修正。
+update p_template_map set TEMA_FIELD='blPaymentTerm' where tety_id=1 and tema_name='付费方式' and TEMA_FIELD='blValuePayment';
--- FOS/sql/fos_template_map.sql
+++ FOS/sql/fos_template_map.sql
@@ -130,7 +130,7 @@ INSERT INTO `P_TEMPLATE_MAP` (`TETY_ID`, `TEMA_NAME`, `TEMA_TABLE`, `TEMA_FIELD`
 (1, '到付总额', 'FBl', 'blTotalCc', NULL),
 (1, '会计信息', 'FBl', 'blAccountingInfo', NULL),
 (1, 'IATA', 'FBl', 'blAgentIataCode', NULL),
-(1, '付费方式', 'FBl', 'blValuePayment', 'overridePP'),
+(1, '付费方式', 'FBl', 'blPaymentTerm', 'overridePP'),
 (1, '付款方式', 'FBl', 'blValuePayment', NULL),
 (1, '其他付款方式', 'FBl', 'blOtherPayment', NULL),
 (1, '预付其他费用', 'FBl', 'blOtherDcPp', NULL),
