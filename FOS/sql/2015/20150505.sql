1ed25fa088c80215917d3fe4b7427dd6074fb989
yinyulong <yulong.yin@hitisoft.com>
2015-05-05 10:44:49 +0800

    装箱通知模板添加字段。

---
+++ FOS/sql/2015/20150505.sql
@@ -0,0 +1,5 @@
+-- add by yinyulong
+INSERT INTO `P_TEMPLATE_MAP` (`TETY_ID`, `TEMA_NAME`, `TEMA_TABLE`, `TEMA_FIELD`, `TEMA_CONVERTER`) VALUES
+(18, '箱公司', 'FConsign', 'consContainerCompanyName', NULL),
+(18, '委托日期', 'FConsign', 'consDate', NULL),
+(18, 'MBLNo', 'FConsign', 'consMblNo', NULL);
